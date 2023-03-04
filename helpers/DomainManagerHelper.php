f81aadb82349ee8c4ef1850db01203801876eb89
Firdavs Ibodullaev <firdavs3253@gmail.com>
2023-03-04 15:18:38 +0500

    added mostbyte manager helper

--- helpers/DomainManagerHelper.php
+++ helpers/DomainManagerHelper.php
@@ -1,11 +1,18 @@
 <?php
 
 use Mostbyte\Multidomain\Managers\DomainManager;
+use Mostbyte\Multidomain\Managers\Manager;
 
-if (!function_exists("mostbyteDomainManager"))
-{
-    function mostbyteDomainManager (): DomainManager
+if (!function_exists("mostbyteDomainManager")) {
+    function mostbyteDomainManager(): DomainManager
     {
         return app(DomainManager::class);
+    }
+}
+
+if (!function_exists('mostbyteManager')) {
+    function mostbyteManager(): Manager
+    {
+        return app(Manager::class);
     }
 }
